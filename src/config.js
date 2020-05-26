aa0c23d8564e385ff17418d70b35f4471fed26b3
Simon Rennocks <rennocks@gmail.com>
2020-05-26 19:26:36 -0400

    Initial

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-edcu"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://7k85p8ioea.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_FjqrVA0sh",
+      APP_CLIENT_ID: "notes-app-edcu",
+      IDENTITY_POOL_ID: "us-east-1:e8ce611c-a573-4eb0-af54-67c44d5d71e7"
+    }
+  };
